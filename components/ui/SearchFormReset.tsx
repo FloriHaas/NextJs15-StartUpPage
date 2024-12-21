b38a5508ed30a2f7ed6e7870b094b6b31b543fb6
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-21 15:50:02 +0100

    Use Shadcn Buttons for search bar

--- components/ui/SearchFormReset.tsx
+++ components/ui/SearchFormReset.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import React from "react";
+import { X } from "lucide-react";
 
 const SearchFormReset = () => {
   const reset = () => {
@@ -13,7 +14,7 @@ const SearchFormReset = () => {
   return (
     <button type="reset" onClick={reset}>
       <Link href="/" className="search-btn text-white">
-        X
+        <X className="size-5" />
       </Link>
     </button>
   );
