3f99ff51fbd27e9482f66bbfe941caaf70bff2d5
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-23 17:36:50 +0100

    Add JWT and session handling for GitHub authentication integration

--- auth.ts
+++ auth.ts
@@ -3,7 +3,7 @@ import GitHub from "next-auth/providers/github";
 import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
 import { client } from "@/sanity/lib/client";
 import { writeClient } from "@/sanity/lib/write-client";
- 
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
   callbacks: {
@@ -31,5 +31,22 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 
       return true;
     },
-  }
-})
+    async jwt({ token, account, profile }) {
+      if (account && profile) {
+        const user = await client
+          .withConfig({ useCdn: false })
+          .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+            id: profile?.id,
+          });
+
+        token.id = user?._id;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      Object.assign(session, { id: token.id });
+      return session;
+    },
+  },
+});
